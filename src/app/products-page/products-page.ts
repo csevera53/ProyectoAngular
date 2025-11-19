6234b05b22a90299991230bd511fe29760b2273f
csevera53 <csevera12@gmail.com>
2025-11-19 12:49:38 +0100

    Tabla con imagenes

--- src/app/products-page/products-page.ts
+++ src/app/products-page/products-page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../interfaces/product';
 
 @Component({
   selector: 'products-page',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './products-page.css',
 })
 export class ProductsPage {
-
+  products: Product[] = [{
+    id: 1,
+    description: 'SSD hard drive',
+    available: '2016-10-03',
+    price: 75,
+    imageUrl: '/ssd.jpg',
+    rating: 5
+  },{
+    id: 2,
+    description: 'LGA1151 Motherboard',
+    available: '2016-09-15',
+    price: 96.95,
+    imageUrl: '/motherboard.jpg',
+    rating: 4
+  }];
 }
